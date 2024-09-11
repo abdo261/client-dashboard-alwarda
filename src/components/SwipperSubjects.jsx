c6a47e56f4119ee023d5efedd888c036a964dc5e
abdo261 <abdellah.bs2018@gmail.com>
2024-09-11 14:10:03 +0100

    update  details stueets card counnter

--- src/components/SwipperSubjects.jsx
+++ src/components/SwipperSubjects.jsx
@@ -12,8 +12,13 @@ export default function SwipperSubjects({ subjects = [] }) {
   const progressCircle = useRef(null);
   const progressContent = useRef(null);
   const onAutoplayTimeLeft = (s, time, progress) => {
-    progressCircle.current.style.setProperty("--progress", 1 - progress);
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    if (progressCircle.current) {
+      progressCircle.current.style.setProperty("--progress", 1 - progress);
+    }
+  
+    if (progressContent.current) {
+      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    }
   };
   return (
     <>
@@ -55,7 +60,7 @@ export default function SwipperSubjects({ subjects = [] }) {
             </div>
           </SwiperSlide>
         ))}
-        {subjects.length > 0 && (
+        {subjects.length > 1 && (
           <div className="autoplay-progress" slot="container-end">
             <svg viewBox="0 0 48 48" ref={progressCircle}>
               <circle cx="24" cy="24" r="20"></circle>
